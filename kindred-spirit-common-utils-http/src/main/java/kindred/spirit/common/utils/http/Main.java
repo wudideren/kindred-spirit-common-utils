8e847d56f9303acab7b092739b371576f388d203
jliuw <1605929920@qq.com>
2023-08-27 13:07:50 +0800

    init

---
+++ kindred-spirit-common-utils-http/src/main/java/kindred/spirit/common/utils/http/Main.java
@@ -0,0 +1,12 @@
+package kindred.spirit.common.utils.http;
+
+/**
+ * @author jliuw
+ * @date 2023.08.27
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
